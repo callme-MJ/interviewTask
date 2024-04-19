fe158e891de6966ae91a2f38ba6737aff51b7456
MJ <jabirjbr07@gmail.com>
2024-04-19 07:35:38 +0530

    finaltouch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+export default function App() {
+  const [clicked, setClicked] = useState([]);
 
-function App() {
+  useEffect(() => {
+    let removedCount = 0;
+    const removeLastElement = () => {
+        console.log("started");
+        setTimeout(() => {
+          setClicked(arr => arr.slice(0, arr.length - 1));
+          removedCount++;
+          if (removedCount < 8) {
+            removeLastElement();
+          }
+        }, 1000);
+    };
+if (clicked.length === 8) {
+  removeLastElement();
+}
+  }, [clicked]);
+
+  console.log(clicked);
+  const clickHandler = (ind) => {
+    console.log(ind);
+    if (clicked.includes(ind)) {
+      return;
+    }
+    setClicked([...clicked, ind]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {[...Array(8)].map((_, i) => (
+        <div className="card" onClick={() => clickHandler(i)}>
+          <div
+            className={clicked.includes(i) ? "item bg-green" : "item bg-yellow"}
+          >
+            {i + 1}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
-
-export default App;
